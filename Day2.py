60e30174b394608dad2221af216adc9a8e47937d
Your Name <himalb519@gmail.com>
2026-06-01 07:06:36 +0545

    Day2 duplicates  remove learning

---
+++ Day2.py
@@ -0,0 +1,14 @@
+#tuple
+student_record =("nabaraj",20,87.5,"computer Science")
+print("Student record tuple ",student_record)
+#Set Autometically remove dulicate
+Course_A ={"himal","bob","Rajib","bikalp"}
+Course_B ={"dipak","mukesh","rohan","bikash"}
+print ("course_A all student ",Course_A)
+print ("course_B all student ",Course_B)
+#print("nabaraj"*50)how much the you want print
+print ("course_A all student ",Course_A)
+#remove duplicates from list using set 
+scores_with_duplicates =[85,92,78,95,85]
+unique_score = list(set(scores_with_duplicates))
+print("\nOriginal data ",unique_score)
